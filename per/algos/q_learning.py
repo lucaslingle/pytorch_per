a3cb5462f888c0ed8545224c36ca505e4c183d99
lucaslingle <lucasdaxlingle@gmail.com>
2021-12-02 14:38:11 -0800

    commit updates

--- per/algos/q_learning.py
+++ per/algos/q_learning.py
@@ -87,18 +87,18 @@ def training_loop(
 ):
     o_t = env.reset()
     for t in range(t0, max_env_steps_per_process):
-        # maybe update target network
+        ### maybe update target network
         if t > 0 and t % target_update_interval == 0:
             update_target_network(
                 q_network=q_network,
                 target_network=target_network)
 
-        # update annealed statistics
+        ### update annealed statistics.
         epsilon_t = epsilon_anneal_fn(t, max_env_steps_per_process)
         alpha_t = alpha_annealing_fn(t, max_env_steps_per_process)
         beta_t = beta_annealing_fn(t, max_env_steps_per_process)
 
-        # act
+        ### act.
         a_t = q_network.sample(
             x=tc.FloatTensor(o_t).unsqueeze(0), epsilon=epsilon_t)
         a_t = a_t.squeeze(0).detach().numpy()
@@ -111,12 +111,12 @@ def training_loop(
         experience_tuple_t = ExperienceTuple(
             s_t=o_t, a_t=a_t, r_t=r_t, d_t=d_t, s_tp1=o_tp1, td_err=None)
 
-        # update replay memory
+        ### update replay memory.
         replay_memory.update_alpha(alpha_t)
         replay_memory.update_beta(beta_t)
         replay_memory.insert(experience_tuple_t)
 
-        # maybe learn
+        ### maybe learn.
         if t > 0 and t % num_env_steps_per_policy_update == 0:
             # TODO(lucaslingle):
             #      check replay memory has at least min entries before learning,
