94746f818358f85b1dc00c4b4e1dffffbe4b7592
lucaslingle <lucasdaxlingle@gmail.com>
2021-12-07 21:27:00 -0800

    commit updated script

--- script.py
+++ script.py
@@ -34,27 +34,27 @@ def create_argparser():
     parser.add_argument("--optimizer_name", choices=['rmsprop', 'adam', 'sgd'], default='rmsprop')
     parser.add_argument("--learning_rate", type=float, default=8 * 6.25e-5)
     parser.add_argument("--huber_loss", type=int, choices=[0,1], default=1)
-    parser.add_argument("--global_target_update_interval", type=int, default=1e4)
-    parser.add_argument("--global_max_env_steps", type=int, default=50e6)
-    parser.add_argument("--global_num_env_steps_per_policy_update", type=int, default=4*8)
-    parser.add_argument("--global_num_env_steps_before_learning", type=int, default=5e4)
+    parser.add_argument("--target_update_interval", type=int, default=1e4)
+    parser.add_argument("--max_env_steps_per_process", type=int, default=50e6)
+    parser.add_argument("--num_env_steps_per_policy_update", type=int, default=4*8)
+    parser.add_argument("--num_env_steps_before_learning", type=int, default=5e4)
     parser.add_argument("--batches_per_policy_update", type=int, default=1)
-    parser.add_argument("--global_batch_size", type=int, default=32*8)
+    parser.add_argument("--batch_size", type=int, default=32*8)
     parser.add_argument("--discount_gamma", type=float, default=0.99)
     parser.add_argument("--epsilon_init", type=float, default=1.0)
     parser.add_argument("--epsilon_final", type=float, default=0.1)
     parser.add_argument("--epsilon_annealing", type=int, choices=[0,1], default=1)
-    parser.add_argument("--global_epsilon_annealing_start_step", type=int, default=0)
-    parser.add_argument("--global_epsilon_annealing_end_step", type=int, default=1e6)
+    parser.add_argument("--epsilon_annealing_start_step", type=int, default=0)
+    parser.add_argument("--epsilon_annealing_end_step", type=int, default=1e6)
 
     ### replay hparams
-    parser.add_argument("--global_replay_memory_size", type=int, default=1e6)
+    parser.add_argument("--replay_memory_size", type=int, default=1e6)
     parser.add_argument("--alpha_init", type=float, default=0.6)
     parser.add_argument("--beta_init", type=float, default=0.4)
     parser.add_argument("--alpha_annealing", type=int, choices=[0,1], default=0)
     parser.add_argument("--beta_annealing", type=int, choices=[0,1], default=1)
-    parser.add_argument("--global_alpha_annealing_start_step", type=int, default=0)
-    parser.add_argument("--global_beta_annealing_start_step", type=int, default=0)
+    parser.add_argument("--alpha_annealing_start_step", type=int, default=0)
+    parser.add_argument("--beta_annealing_start_step", type=int, default=0)
 
     ### checkpointing
     parser.add_argument("--checkpoint_dir", type=str, default='models_dir/')
@@ -165,7 +165,7 @@ def create_annealing_fn(
 
 def main():
     args, comm = create_argparser().parse_args(), get_comm()
-    args = maybe_load_config(args, comm)
+    #args = maybe_load_config(args, comm)
 
     ### create env.
     env = create_env(
@@ -189,14 +189,14 @@ def main():
     scheduler = create_scheduler()
 
     replay_memory = create_replay_memory(
-        replay_memory_size=int(args.global_replay_memory_size) // comm.Get_size(),
+        replay_memory_size=int(args.replay_memory_size),
         alpha_init=args.alpha_init,
         beta_init=args.beta_init)
 
     ### maybe load checkpoint on process with rank zero.
-    num_local_env_steps_thus_far = 0
+    num_env_steps_thus_far = 0
     if comm.Get_rank() == ROOT_RANK:
-        num_local_env_steps_thus_far = maybe_load_checkpoint(
+        num_env_steps_thus_far = maybe_load_checkpoint(
             checkpoint_dir=args.checkpoint_dir,
             run_name=args.run_name,
             q_network=q_network,
@@ -207,7 +207,7 @@ def main():
 
     if args.mode == 'train':
         ### sync torch checkpointables' states across processes.
-        num_local_env_steps_thus_far = comm.bcast(num_local_env_steps_thus_far, root=ROOT_RANK)
+        num_env_steps_thus_far = comm.bcast(num_env_steps_thus_far, root=ROOT_RANK)
         sync_state(
             q_network=q_network,
             target_network=target_network,
@@ -219,7 +219,7 @@ def main():
         ### maybe load replay memory on each process.
         if bool(args.replay_checkpointing):
             replay_memory = maybe_load_replay_memory(
-                steps=num_local_env_steps_thus_far,
+                steps=num_env_steps_thus_far,
                 checkpoint_dir=args.checkpoint_dir,
                 run_name=args.run_name,
                 rank=comm.Get_rank(),
@@ -230,20 +230,20 @@ def main():
             initial_value=args.alpha_init,
             final_value=0.0,
             do_annealing=bool(args.alpha_annealing),
-            start_step=int(args.global_alpha_annealing_start_step) // comm.Get_size(),
-            end_step=int(args.global_max_env_steps) // comm.Get_size())
+            start_step=int(args.alpha_annealing_start_step),
+            end_step=int(args.max_env_steps_per_process))
         beta_annealing_fn = create_annealing_fn(
             initial_value=args.beta_init,
             final_value=1.0,
             do_annealing=bool(args.beta_annealing),
-            start_step=int(args.global_beta_annealing_start_step) // comm.Get_size(),
-            end_step=int(args.global_max_env_steps) // comm.Get_size())
+            start_step=int(args.beta_annealing_start_step),
+            end_step=int(args.max_env_steps_per_process))
         epsilon_annealing_fn = create_annealing_fn(
             initial_value=args.epsilon_init,
             final_value=args.epsilon_final,
             do_annealing=bool(args.epsilon_annealing),
-            start_step=int(args.global_epsilon_annealing_start_step) // comm.Get_size(),
-            end_step=int(args.global_epsilon_annealing_end_step) // comm.Get_size())
+            start_step=int(args.epsilon_annealing_start_step),
+            end_step=int(args.epsilon_annealing_end_step))
 
         ### run it!
         training_loop(
@@ -253,13 +253,13 @@ def main():
             optimizer=optimizer,
             scheduler=scheduler,
             target_network=target_network,
-            target_update_interval=int(args.global_target_update_interval) // comm.Get_size(),
-            max_env_steps_per_process=int(args.global_max_env_steps) // comm.Get_size(),
-            num_env_steps_per_policy_update=int(args.global_num_env_steps_per_policy_update) // comm.Get_size(),
-            num_env_steps_before_learning=int(args.global_num_env_steps_before_learning) // comm.Get_size(),
-            num_env_steps_thus_far=num_local_env_steps_thus_far,
+            target_update_interval=int(args.target_update_interval),
+            max_env_steps_per_process=int(args.max_env_steps_per_process),
+            num_env_steps_per_policy_update=int(args.num_env_steps_per_policy_update),
+            num_env_steps_before_learning=int(args.num_env_steps_before_learning),
+            num_env_steps_thus_far=num_env_steps_thus_far,
             batches_per_policy_update=args.batches_per_policy_update,
-            batch_size=args.global_batch_size // comm.Get_size(),
+            batch_size=args.batch_size,
             alpha_annealing_fn=alpha_annealing_fn,
             beta_annealing_fn=beta_annealing_fn,
             epsilon_annealing_fn=epsilon_annealing_fn,
